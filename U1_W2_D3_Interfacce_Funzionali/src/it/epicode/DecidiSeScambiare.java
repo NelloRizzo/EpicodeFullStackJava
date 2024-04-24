5c325f00477545c9c1c63c5fd72a035530635ec6
Nello Rizzo <nellorizzo@live.it>
2024-04-24 12:12:31 +0200

    Lezione del 24/04

---
+++ U1_W2_D3_Interfacce_Funzionali/src/it/epicode/DecidiSeScambiare.java
@@ -0,0 +1,10 @@
+package it.epicode;
+
+//public abstract class DecidiSeScambiare {
+//	abstract boolean decidiSeScambiare(int a, int b);
+//}
+
+// INTERFACCIA FUNZIONALE
+public interface DecidiSeScambiare{
+	boolean decidiSeScambiare(int a, int b);
+}
