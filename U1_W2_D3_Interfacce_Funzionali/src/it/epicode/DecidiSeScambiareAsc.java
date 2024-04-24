795f59701bd7aa8f201d10ac38dc8fb58d47d8ee
Nello Rizzo <nellorizzo@live.it>
2024-04-24 12:15:23 +0200

    Revert "Lezione del 24/04"
    This reverts commit baae5b13156cbbb5e9d61038d1a11f5730c046d6.

--- U1_W2_D3_Interfacce_Funzionali/src/it/epicode/DecidiSeScambiareAsc.java
+++
@@ -1,9 +0,0 @@
-package it.epicode;
-
-//public class DecidiSeScambiareAsc extends DecidiSeScambiare{
-public class DecidiSeScambiareAsc implements DecidiSeScambiare {
-	@Override
-	public boolean decidiSeScambiare(int a, int b) {
-		return a > b;
-	}
-}
--- U1_W2_D3_Interfacce_Funzionali/src/it/epicode/DecidiSeScambiareDesc.java
+++
@@ -1,7 +0,0 @@
-package it.epicode;
-
-public class DecidiSeScambiareDesc implements DecidiSeScambiare {
-	public boolean decidiSeScambiare(int a, int b) {
-		return a < b;
-	}
-}
