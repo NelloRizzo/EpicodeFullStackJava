21ee52dbd42fb94567262ba1630bd6e6b2f842ef
Nello Rizzo <nellorizzo@live.it>
2024-04-26 12:52:48 +0200

    Fine esempio di persistenza su file.

--- U1_W2_D4_Servizi/src/it/epicode/Program.java
+++ U1_W2_D4_Servizi/src/it/epicode/Program.java
@@ -13,10 +13,17 @@ public class Program {
 	static final String FILE_NAME = "E:\\Codice\\EpicodeFS0124\\Workspace\\U1_W2_D4_Servizi\\target\\comuni.csv";
 
 	public static void main(String[] args) {
-		CityService service = new FileCityService(FILE_NAME);
+		// FileCityService service = new FileCityService(FILE_NAME); // legge i dati da un csv
+		CityService service = new FileCityService(); // legge i dati da uno storage interno
 
 		service.getCities().stream() //
 				.filter(c -> c.getAcronym().equals("RM")).forEach(c -> logger.debug("{}", c));
+
+		// service.save();
+
+		var aglie = service.getCities().stream() //
+				.filter(c -> c.getId() == 1001).findFirst().orElseThrow();
+		service.deleteCity(aglie);
 
 	}
 
--- U1_W2_D4_Servizi/src/it/epicode/services/FileCityService.java
+++ U1_W2_D4_Servizi/src/it/epicode/services/FileCityService.java
@@ -1,12 +1,15 @@
 package it.epicode.services;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -14,8 +17,10 @@ import it.epicode.data.City;
 
 public class FileCityService implements CityService {
 	private static final Logger logger = LoggerFactory.getLogger(FileCityService.class);
-	
+
 	private final ArrayList<City> cities = new ArrayList<>();
+
+	private static final String STORAGE = "./cities.my";
 
 	public FileCityService(String file) {
 		try {
@@ -32,16 +37,57 @@ public class FileCityService implements CityService {
 		}
 	}
 
+	public FileCityService() {
+		load();
+	}
+
+	public void save() {
+		File f = new File(STORAGE);
+		try {
+			FileUtils.delete(f);
+		} catch (IOException e) {
+			logger.error("Eccezione durante l'eliminazione del file di storage", e);
+		}
+		cities.stream().forEach(c -> {
+			try {
+				var lines = Arrays.asList(c.getId().toString(), c.getAcronym(), c.getName(), c.getProvince());
+				FileUtils.writeLines(f, StandardCharsets.ISO_8859_1.name(), lines, true);
+			} catch (IOException e) {
+				logger.error("Eccezione durante il salvataggio", e);
+			}
+		});
+	}
+
+	private void load() {
+		File f = new File(STORAGE);
+		try {
+			var l = FileUtils.readLines(f, StandardCharsets.ISO_8859_1);
+			for (int i = 0; i < l.size(); i += 4) {
+				cities.add( //
+						new City( //
+								Long.parseLong(l.get(i)), //
+								l.get(i + 2), //
+								l.get(i + 1), //
+								l.get(i + 3)));
+			}
+		} catch (IOException e) {
+			logger.error("Eccezione durante il caricamento", e);
+		}
+	}
+
 	@Override
 	public void addCity(City c) {
 		cities.add(c);
+		save();
 	}
 
 	@Override
 	public void deleteCity(City c) {
 		var city = cities.stream().filter(e -> e.getId() == c.getId()).findAny();
-		if (city.isPresent())
+		if (city.isPresent()) {
 			cities.remove(city.get());
+			save();
+		}
 	}
 
 	@Override
